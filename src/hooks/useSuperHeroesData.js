7e3e6a1ad41497677203e5c5b510f2c4709dfff9
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-08-04 15:41:21 +0300

    Optimistic Updates

--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -24,11 +24,25 @@ export const useSuperHeroesData = ({ onSuccess, onError }) => {
 export const useAddSuperHeroData = () => {
   const queryClient = useQueryClient();
   return useMutation(addSuperHero, {
-    onSuccess: (data) => {
-      // queryClient.invalidateQueries("super-heroes");
+    // onSuccess: (data) => {
+    //   queryClient.setQueryData("super-heroes", (oldQueryData) => {
+    //     return { ...oldQueryData, data: [...oldQueryData.data, data.data] };
+    //   });
+    // },
+    onMutate: async (newHero) => {
+      await queryClient.cancelQueries("super-heroes");
+      const previuosHeroData = queryClient.getQueryData("super-heroes");
       queryClient.setQueryData("super-heroes", (oldQueryData) => {
-        return { ...oldQueryData, data: [...oldQueryData.data, data.data] };
+        return { ...oldQueryData, data: [...oldQueryData.data, newHero] };
       });
+
+      return { previuosHeroData };
+    },
+    onError: (_error, _hero, context) => {
+      queryClient.setQueryData("super-heroes", context.previuosHeroData);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries("super-heroes");
     },
   });
 };
